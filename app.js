a9c882f26d466047c5c480945bd8710b3171fc90
sourov halder <sourovhalder97@gmail.com>
2022-02-25 00:22:12 +0600

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//setting my initial value
+let count = 0;
+
+//select value and buttons
+
+const value = document.querySelector('#value');
+const btns =document.querySelectorAll(".btn");
+
+console.log(btns); 
